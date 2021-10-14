deb9f5ef8b26e7367fdaddf035aa3c671e43e4d7
slwhswk <87063105+wkdtpzld@users.noreply.github.com>
2021-10-14 21:01:50 +0900

    PACTH

--- 10001~20000/10971.py
+++ 10001~20000/10971.py
@@ -1,20 +1,18 @@
 import sys
+
 n=int(input())
-
-s=[list(map(int,sys.stdin.readline().split())) for i in range(n)]
-
-min_value = sys.maxsize
-visited=[False for _ in range(n)]
+s=[list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+visited=[False]*n
+min_value=sys.maxsize
 
 def dfs(start,cur,cost):
-    global  s, visited, min_value
+    global visited, s, min_value
 
     if start == cur and visited.count(False) == 0:
         min_value = min(min_value,cost)
 
-
     for i in range(n):
-        if not s[cur][i] == 0 and not visited[i]:
+        if visited[i]==False and s[cur][i] != 0 :
             visited[i] = True
             dfs(start,i,cost+s[cur][i])
             visited[i] = False
