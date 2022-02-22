76c08e9be7fd5925ded80d609ee4ca2ad5b63975
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2022-02-22 10:21:35 +0900

    ABCDE 문제풀이 오류
    풀지도 않은걸 올려버렸음

--- 10001~20000/13023 ABCDE.py
+++ 10001~20000/13023 ABCDE.py
@@ -1,9 +1,27 @@
-n,m= map(int,input().split())
+n, m = map(int, input().split())
 
-visited = [[0]*n for i in range(n)]
+arr = [[] for i in range(n)]
+visited = [0] * n
 
 for i in range(m):
-    a,b = map(int,input().split())
-    visited[a][b] = 1
+    a, b = map(int, input().split())
+    arr[a].append(b)
+    arr[b].append(a)
 
-print(visited)
+def dfs(idx, x):
+    if idx == 4:
+        print(1)
+        exit()
+
+    for i in arr[x]:
+        if visited[i] == 0:
+            visited[i] = 1
+            dfs(idx+1,i)
+            visited[i] = 0
+
+for i in range(n):
+    visited[i] = 1
+    dfs(0,i)
+    visited[i] = 0
+
+print(0)
