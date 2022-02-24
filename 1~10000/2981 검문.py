87a39552b9998e41f689fa13ee96aa9cba030e40
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2022-02-24 10:22:16 +0900

    Update 2981 검문.py

--- 1~10000/2981 검문.py
+++ 1~10000/2981 검문.py
@@ -1,10 +1,29 @@
+import math
+
 n = int(input())
 
-s = [int(input()) for i in range(n)]
+s=[]
 
-def gcd(a,b):
+answer = []
+gcd = 0
+for i in range(n):
+    s.append(int(input()))
+    if i == 1:
+        gcd = abs(s[1]-s[0])
 
-    while b > 0:
-        a,b = b, a%b
-    return a
+    gcd = math.gcd(abs(s[i]-s[i-1]),gcd)
 
+gcd_a = int(gcd ** 0.5)
+
+for i in range(2,gcd_a+1):
+    if gcd % i == 0:
+        answer.append(i)
+        answer.append(gcd // i)
+
+answer.append(gcd)
+
+answer = list(set(answer))
+
+answer.sort()
+
+print(' '.join(map(str,answer)))
